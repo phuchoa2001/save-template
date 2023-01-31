c1c0b160d91ec77ab03951583cd167840b29c4cd
phuchoa <phuchoa1202@gmail.com>
2023-01-31 21:12:04 +0700

    Hoa/fix_layout_va_space_2

--- src/components/Layout/layoutMobile/LayoutMobile.tsx
+++ src/components/Layout/layoutMobile/LayoutMobile.tsx
@@ -1,9 +1,16 @@
 import { FC, memo } from 'react'
 import { Outlet } from 'react-router-dom'
 
+// ** Styles
+import { toStyledModuleNames } from '@utils/styledModuleName';
+import styles from './styles.module.scss'
+const styledModule = toStyledModuleNames(styles);
+
 const MobileLayout: FC = () => {
   return (
-    <Outlet />
+    <div className={styledModule`main-mobile`}>
+      <Outlet />
+    </div>
   )
 }
 
