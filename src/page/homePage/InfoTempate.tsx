0e8ae667afee41a2d3814d762cd85c7c18eed045
phuchoa <phuchoa1202@gmail.com>
2023-02-02 21:00:28 +0700

    Hoa/seo_cho_cac_page

--- src/page/homePage/InfoTempate.tsx
+++ src/page/homePage/InfoTempate.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { useParams } from "react-router-dom";
 
 import Container from '@components/Layout/layoutMobile/Container'
+import HeaderSeo from '@components/Seo/HeaderSeo'
 
 type Props = {}
 
 const InfoTempate = (props: Props) => {
+  const { templateId } = useParams();
+
   return (
-    <Container label="Thông tin Tempalte">
-      <div>Page InfoTempate</div>
-    </Container>
+    <Fragment>
+      <HeaderSeo
+        title={"Template: " + templateId}
+        desc={process.env.REACT_APP_PAGE_DESCRIPTION}
+        image={process.env.REACT_APP_PAGE_IMAGE}
+      />
+      <Container label="Thông tin Tempalte">
+        <div>Page InfoTempate</div>
+      </Container>
+    </Fragment>
   )
 }
 
