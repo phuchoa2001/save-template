58eea08cb9ed436041c0e06ab887213436c1c72c
Đặng Phúc Hòa <95266292+phuchoa2001@users.noreply.github.com>
2023-02-07 20:57:09 +0700

    Hoa/them_lorem_va_an_menu_page_info (#22)

--- src/page/homePage/InfoTempate.tsx
+++ src/page/homePage/InfoTempate.tsx
@@ -16,8 +16,26 @@ const InfoTempate = (props: Props) => {
         desc={process.env.REACT_APP_PAGE_DESCRIPTION}
         image={process.env.REACT_APP_PAGE_IMAGE}
       />
-      <Container label="Thông tin Tempalte">
-        <div>Page InfoTempate</div>
+      <Container label="Thông tin Tempalte" hideMenu={true}>
+        <div>Template : {templateId}</div>
+        <div>Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Architecto repellat aliquam
+          perferendis aut suscipit asperiores veritatis
+          est illo non, ullam mollitia sapiente iure
+          laborum optio maxime molestias voluptatibus omnis! Odio?
+        </div>
+        <div>Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Architecto repellat aliquam
+          perferendis aut suscipit asperiores veritatis
+          est illo non, ullam mollitia sapiente iure
+          laborum optio maxime molestias voluptatibus omnis! Odio?
+        </div>
+        <div>Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Architecto repellat aliquam
+          perferendis aut suscipit asperiores veritatis
+          est illo non, ullam mollitia sapiente iure
+          laborum optio maxime molestias voluptatibus omnis! Odio?
+        </div>
       </Container>
     </Fragment>
   )
